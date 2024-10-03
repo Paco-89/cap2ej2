61411586f3bf821edbde4548d2721ccdc3bffaab
Paco-89 <flyingv89@gmail.com>
2024-10-03 14:34:54 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args ){
+        System.out.println("Francisco Cobo Sánchez");
+        System.out.println("Vega del mar,8,San Pedro");
+        System.out.println("Málaga");
+        System.out.printf("%-10s %-10s %n", "computer", "ordenador");
+        System.out.printf("%-10s %-10s %n", "student", "alumno/a");
+        System.out.printf("%-10s %-10s %n","cat", "gato" );
+        System.out.printf("%-10s %-10s %n", "penguin", "pingüino");
+        System.out.printf("%-10s %-10s %n", "machine", "máquina");
+        System.out.printf("%-10s %-10s %n", "nature", "naturaleza");
+        System.out.printf("%-10s %-10s %n", "light", "luz");
+        System.out.printf("%-10s %-10s %n", "green", "verde");
+        System.out.printf("%-10s %-10s %n", "book", "libro");
+        System.out.printf("%-10s %-10s %n %n", "pyramid", "piramide");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %n %n", "LUN", "MAR", "MIE", "JUE", "VIE");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %n", "PROG", "IPEI", "PROG", "LM", "LM");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %n", "PROG", "IPEI", "PROG", "LM", "BD");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %n", "SI", "IPEI", "PROG", "BD", "BD");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %n", "SI", "PROG", "DIG", "BD", "SI");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %n","BD", "PROG", "ED", "SOS", "SI");
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s %n", "BD", "PROG", "ED", "ED", "SI"); 
+        System.out.printf("%30s %n", "Tuco Surf Shop");
+        System.out.printf("%38s %n", "Placeta San Hermenegildo, nº3");
+        System.out.printf("%31s %n", "Alquife, Granada");
+        System.out.println( "**************************************************");
+        System.out.println( "********************Factura001********************");
+        System.out.println( "**************************************************");
+        String producto = "Cera";
+        String producto2 = "Tabla";
+        int cantidad = 5;
+        int cantidad2 = 1;
+        double precio = 3.25;
+        double precio2 = 359.65;
+        System.out.printf("%-28s %-10s %10s %n", "Producto", "Cantidad", "Precio"); 
+        System.out.printf("%-35s %-9s %-10s %n", producto, cantidad, precio);
+        System.out.printf("%-35s %-9s %-10s %n", producto2, cantidad2, precio2);
+        System.out.println ("Total:                                           x");
+
+
+
+
+
+    
+    }
+
+}
